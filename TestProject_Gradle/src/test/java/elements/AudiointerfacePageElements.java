3b4ee789664f008033756fced4190467cce11d12
Irina Evsei <ie.irinaevsei@gmail.com>
2018-10-30 15:34:46 +0300

    Updated locators.

--- TestProject_Gradle/src/test/java/elements/AudiointerfacePageElements.java
+++ TestProject_Gradle/src/test/java/elements/AudiointerfacePageElements.java
@@ -4,6 +4,5 @@ import org.openqa.selenium.By;
 
 public class AudiointerfacePageElements {
     public static By AUDIOINTERFACE_MIN_PRICE = By.xpath("//*[@id=\"schema-filter\"]/div[1]/div[4]/div[2]/div/div[1]/input");
-           // ("//div[@class='schema-filter__group']//input[contains(@placeholder,'от')])[1]");
     public static By MIN_PRICE_TAG = By.xpath("//*[@id=\"schema-tags\"]/div/span");
 }
--- TestProject_Gradle/src/test/java/elements/CatalogNavigationPageElements.java
+++ TestProject_Gradle/src/test/java/elements/CatalogNavigationPageElements.java
@@ -4,6 +4,6 @@ import org.openqa.selenium.By;
 
 public class CatalogNavigationPageElements {
     public static By CATALOG_NAVIGATION_LIST = By.xpath("//div[@class='catalog-navigation-list__aside-list']//div[contains(., 'Мультимедиа периферия')]");
-    public static By CATALOG_NAVIGATION_LIST_ITEM = By.xpath("//*[@id=\"container\"]/div/div[2]/div/div/div[1]/div[2]/div/div[2]/div[1]/div/div[6]/div[2]/div/a[5]"); //???
+    public static By CATALOG_NAVIGATION_LIST_ITEM = By.xpath("//div[@class='catalog-navigation-list__aside-item catalog-navigation-list__aside-item_active']//a[contains(@href, 'audiointerface')]"); //???
     public static By AUDIOINTERFACE_PAGE_TITLE = By.xpath("//div[@class='schema-header']//h1[contains(., 'Аудиоинтерфейсы')]");
 }
